033666dd013bcb166db0b3ff02001657cb9c7ce3
lginog <gesualdoluigi2000@gmail.com>
2023-05-06 22:19:54 +0200

    Balancer aggiunto (problema decremento volume canale destro)

--- Lab3_Audio.srcs/sources_1/new/balance_controller.vhd
+++ Lab3_Audio.srcs/sources_1/new/balance_controller.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -49,7 +49,107 @@ end balance_controller;
 
 architecture Behavioral of balance_controller is
 
+    type state_type is (IDLE, RECEIVE, TRANSMIT_L, TRANSMIT_R);
+
+    signal state : state_type;
+    
+    signal data : std_logic_vector(s_axis_tdata'range);
+    
+    signal shift : unsigned(2 downto 0);
+    signal l_r : std_logic;
+    
 begin
+
+    with state select s_axis_tready <=
+        '0' when IDLE,
+        '1' when RECEIVE,
+        '0' when TRANSMIT_L,
+        '0' when TRANSMIT_R;
+   
+    with state select m_axis_tvalid <=
+        '0' when IDLE,
+        '0' when RECEIVE,
+        '1' when TRANSMIT_L,
+        '1' when TRANSMIT_R;
+        
+    with state select m_axis_tlast <=
+        '0' when IDLE,
+        '0' when RECEIVE,
+        '0' when TRANSMIT_L,
+        '1' when TRANSMIT_R;        
+
+    process(aclk, aresetn)
+    
+    begin
+    
+        if aresetn = '0' then
+            state <= IDLE;
+            data <= (Others => '0');
+            shift <= (Others => '0');
+            l_r <= '0';
+        elsif rising_edge(aclk) then
+            case state is
+                
+                when IDLE =>
+                    state <= receive;
+                    shift <= unsigned(balance(8 downto 6));
+                    l_r <= balance(9);
+                    
+                when RECEIVE =>
+                    if s_axis_tvalid = '1' and s_axis_tlast = '0' then
+                        data <= s_axis_tdata;
+                    elsif s_axis_tvalid = '1' and s_axis_tlast = '1' then
+                        data <= s_axis_tdata;
+                        shift <= (not shift) + 1;
+                        
+                        if l_r = '1' then
+                            
+                            for I in 0 to s_axis_tdata'high loop
+                                if I >= to_integer(shift) then
+                                    m_axis_tdata(I) <= '0';
+                                    m_axis_tdata(I-to_integer(shift)) <= data(I);
+                                end if;
+                            end loop;
+                            
+                        elsif l_r = '0' then
+                            m_axis_tdata <= data;
+                        end if;
+                        
+                        state <= TRANSMIT_L;
+                    end if;               
+                                                     
+                when TRANSMIT_L =>
+                    if m_axis_tready = '1' then
+                        if l_r = '0' then
+                            
+                            for I in 0 to s_axis_tdata'high loop
+                                if I >= to_integer(shift) then
+                                    m_axis_tdata(I) <= '0';
+                                    m_axis_tdata(I-to_integer(shift)) <= data(I);
+                                end if;
+                            end loop;
+                            
+                        elsif l_r = '1' then
+                            m_axis_tdata <= data;
+                        end if;
+                        
+                        state <= TRANSMIT_R;
+                    end if;
+                    
+                when TRANSMIT_R => 
+                    if m_axis_tready = '1' then
+                        state <= RECEIVE;
+                        shift <= unsigned(balance(8 downto 6));
+                        l_r <= balance(9);
+                    end if;
+                    
+                when others => 
+                    state <= IDLE;
+                    
+            end case;   
+        end if;
+        
+    end process;
 
 
 end Behavioral;
