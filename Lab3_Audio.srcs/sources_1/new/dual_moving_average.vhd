c2e8da7e55f7aaf8c518c1fb98e85e456e6d3775
Andrea <andr3a759@gmail.com>
2023-05-09 15:29:15 +0200

    dma

--- Lab3_Audio.srcs/sources_1/new/dual_moving_average.vhd
+++ Lab3_Audio.srcs/sources_1/new/dual_moving_average.vhd
@@ -50,7 +50,8 @@ end dual_moving_average;
 architecture Behavioral of dual_moving_average is
     type state_t is (RCV_L, RCV_R, SEND_L, SEND_R);
     type ring_buffer is array (31 downto 0) of unsigned (23 downto 0);
-    signal state : state_t;
+    signal state_rx : state_t;
+    signal state_tx : state_t;
     signal ring_buffer_entry : unsigned (5 downto 0) := (others => '0');
     signal ring_buffer_read : unsigned (5 downto 0) := (others => '0');
     signal filtered_l : unsigned (23 downto 0) := (others => '0');
@@ -61,58 +62,74 @@ architecture Behavioral of dual_moving_average is
     signal filter_in_l : ring_buffer := ( others => ( others => '0'));
 begin
     
-    state_seq: process(aclk, aresetn)
+    state_seq_rx: process(aclk, aresetn)
     begin
         if aresetn = '0' then
-            state <= RCV_L;
+            state_rx <= RCV_L;
+            filter_in_l <= (others => (others => '0'));
+            filter_in_r <= (others => (others => '0'));
+            ring_buffer_read <= (others => '0');
         elsif rising_edge(aclk) then
-            case state is
+            case state_rx is
                 when RCV_L =>
                     s_axis_tready <= '1';
                     if s_axis_tvalid = '1' and s_axis_tlast = '0' then
-                        filter_in_l(TO_INTEGER(ring_buffer_entry)) <= unsigned(s_axis_tdata);
-                        state <= RCV_R;
+                        filter_in_l(TO_INTEGER(ring_buffer_entry))      <= unsigned(s_axis_tdata);
+                        state_rx                                        <= RCV_R;
                     end if;
+                
                 when RCV_R =>
                     if s_axis_tvalid = '1' and s_axis_tlast = '1' then
-                        filter_in_r( TO_INTEGER(ring_buffer_entry) ) <= unsigned(s_axis_tdata);
-                        state <= SEND_L;
-                        s_axis_tready <= '0';
+                        filter_in_r( TO_INTEGER(ring_buffer_entry) )    <= unsigned(s_axis_tdata);
+                        state_rx                                          <= RCV_L;
+                        s_axis_tready                                   <= '0';
+                        ring_buffer_entry   <= ring_buffer_entry + 1;
                     end if;
+                when others =>
+                    state_rx <= RCV_L;
+            end case;
+        end if;
+    end process state_seq_rx;
+
+    state_seq_tx: process(aclk, aresetn)
+    begin
+        if aresetn = '0' then
+            state_tx <= RCV_L;
+        elsif rising_edge(aclk) then
+            case state_tx is
                 when SEND_L =>
                     if filter_enable = '1' then
                         m_axis_tdata    <= std_logic_vector(filtered_out_l);
                     else
-                        m_axis_tdata    <= std_logic_vector(filter_in_l(TO_INTEGER(ring_buffer_entry)));
+                        m_axis_tdata    <= std_logic_vector(filter_in_l(TO_INTEGER(ring_buffer_entry - 1)));
                     end if;
-                    m_axis_tvalid <= '1';
+                    m_axis_tvalid   <= '1';
                     if m_axis_tready = '1' then
-                        state <= SEND_R;
+                        state_rx   <= SEND_R;
                     end if;
+                
                 when SEND_R =>
                     if filter_enable = '1' then
                         m_axis_tdata    <= std_logic_vector(filtered_out_r);
                     else
-                        m_axis_tdata    <= std_logic_vector(filter_in_r(TO_INTEGER(ring_buffer_entry)));
+                        m_axis_tdata    <= std_logic_vector(filter_in_r(TO_INTEGER(ring_buffer_entry - 1)));
                     end if;
                     m_axis_tvalid   <= '1';
                     m_axis_tlast    <= '0';
                     if m_axis_tready = '1' then
-                        state <= RCV_R;
-                        ring_buffer_entry <= ring_buffer_entry + 1;
+                        state_rx             <= SEND_L;
                     end if;
                 when others =>
-                    state <= RCV_L;
+                    state_rx <= SEND_L;
             end case;
         end if;
-    end process state_seq;
+    end process state_seq_tx;
 
     filter: process(aclk, aresetn)
     begin
         if aresetn = '0' then
             filtered_out_l <= (others => '0');
             filtered_out_r <= (others => '0');
-            ring_buffer_read <= (others => '0');
         elsif rising_edge(aclk) then
                 filtered_l <= filtered_l + filter_in_l(TO_INTEGER(ring_buffer_read))(23 downto 5);
                 filtered_r <= filtered_r + filter_in_r(TO_INTEGER(ring_buffer_read))(23 downto 5);
